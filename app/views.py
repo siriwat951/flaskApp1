a4cde2b3a922eddf8a3758f2ea51f393040fdd80
siriwat951 <siriwat_tid@cmu.ac.th>
2025-12-01 23:52:15 +0700

    Initial commit

--- app/views.py
+++ app/views.py
@@ -27,3 +27,11 @@ def data():
         "Siriwat": "08-8553-1558"
     }
     return jsonify(d)  # convert your data to JSON and return
+
+
+
+
+@app.route('/lab02')
+def resume():
+    return app.send_static_file('lab02_resume.html')
+
