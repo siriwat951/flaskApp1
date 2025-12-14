357d4c4c2e9928ecc30c4266abe12138f3cc3d13
siriwat951 <siriwat_tid@cmu.ac.th>
2025-12-14 20:26:46 +0700

    send in lab03

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,6 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='static')
 
+app.config['DEBUG'] = True
 
 from app import views  # noqa
--- gunicorn_starter.sh
+++ gunicorn_starter.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-gunicorn --chdir app main:app -w 2 --threads 2 -b 0.0.0.0:8000 --reload
-exec "$@"
+if [ "$FLASK_DEBUG" = "1" ]; then
+    echo "Running on Flask Development Server"
+    cd app || exit 1
+    python3 ../main.py
+else
+    echo "Running on gunicorn"
+    gunicorn main:app -c "$PWD"/gunicorn.config.py
+fi
+exec "$@
