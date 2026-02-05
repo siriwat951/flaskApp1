c2690dc6b67ef10df031259e78e57d178dc57553
siriwat951 <siriwat951@cmu.ac.th>
2026-02-05 15:30:42 +0700

    db connection

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static')
 app.url_map.strict_slashes = False
@@ -9,16 +11,21 @@ app.jinja_options.update({
     'lstrip_blocks': True
 })
 
-app.config['SECRET_KEY'] = \
-    '179810bb56e3c78e5aa5d426aecf3e98432bbe3fd88cf0ac'
-
 app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = \
+    'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 app.config['JSON_AS_ASCII'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Creating an SQLAlchemy instance
+db = SQLAlchemy(app)
 
 @app.before_request
 def remove_trailing_slash():
    # Check if the path ends with a slash but is not the root "/"
     if request.path != '/' and request.path.endswith('/'):
-        return redirect(request.path[:-1], code=308)
+        return redirect(request.path[:-1], code=301)
 
 from app import views  # noqa
---
+++ flask/app/views.py
@@ -0,0 +1,26 @@
+import json
+from flask import (jsonify, render_template,
+                  request, url_for, flash, redirect)
+
+from sqlalchemy.sql import text
+from app import app
+from app import db
+
+@app.route('/')
+def home():
+    return "Flask says 'Hello world!'"
+
+
+@app.route('/db')
+def db_connection():
+    try:
+        with db.engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        return '<h1>db works.</h1>'
+    except Exception as e:
+        return '<h1>db is broken.</h1>' + str(e)
+
+
+@app.route('/lab04')
+def lab04_bootstrap():
+    return app.send_static_file('lab04_bootstrap.html')
