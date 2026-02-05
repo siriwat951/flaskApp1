7116e9193423378dac0260168d31d4c37ea8b272
siriwat951 <siriwat951@cmu.ac.th>
2026-02-05 16:10:22 +0700

    lab10 done

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, redirect
+from werkzeug.debug import DebuggedApplication
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static')
@@ -19,6 +20,9 @@ app.config['JSON_AS_ASCII'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+if app.debug:
+    app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
+    
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
 
