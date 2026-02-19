88eb42c6030a7d52558b96757f4968bae4859f68
siriwat951 <siriwat951@cmu.ac.th>
2026-02-19 19:29:02 +0700

    Lab12

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, request, redirect
+from flask_wtf.csrf import CSRFProtect
 from werkzeug.debug import DebuggedApplication
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__, static_folder='static')
 app.url_map.strict_slashes = False
@@ -17,15 +19,21 @@ app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = \
     'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 app.config['JSON_AS_ASCII'] = False
-
+app.config['GOOGLE_CLIENT_ID'] = os.getenv("GOOGLE_CLIENT_ID", None)
+app.config['GOOGLE_CLIENT_SECRET'] = os.getenv("GOOGLE_CLIENT_SECRET", None)
+app.config['GOOGLE_DISCOVERY_URL'] = os.getenv("GOOGLE_DISCOVERY_URL", None)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL", "sqlite://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 if app.debug:
     app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
     
+csrf = CSRFProtect(app)
+csrf.init_app(app)
+
 # Creating an SQLAlchemy instance
 db = SQLAlchemy(app)
+oauth = OAuth(app)
 
 login_manager = LoginManager()
 login_manager.login_view = 'lab11_login'
--- flask/app/views.py
+++ flask/app/views.py
@@ -2,16 +2,17 @@ from flask import (jsonify, render_template,
                    request, url_for, flash, redirect)
 
 import json
+import secrets
+import string
 from sqlalchemy.sql import text
-from app import app
-from app import db
+from app import app, db, csrf
 from app.models.contact import Contact
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
 from flask_login import login_user, login_required, logout_user, current_user
 from app import login_manager
 from app.models.authuser import AuthUser, PrivateContact
-
+from app import oauth
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -100,6 +101,7 @@ def lab10_phonebook():
 
 
 @app.route("/lab10/contacts")
+@csrf.exempt    
 @login_required
 def lab10_db_contacts():
     # db_contacts = Contact.query.all()
@@ -260,3 +262,68 @@ def gen_avatar_url(email, name):
 def lab11_logout():
     logout_user()
     return redirect(url_for('lab11_index'))
+
+
+@app.route('/google')
+def google():
+
+
+    oauth.register(
+        name='google',
+        client_id=app.config['GOOGLE_CLIENT_ID'],
+        client_secret=app.config['GOOGLE_CLIENT_SECRET'],
+        server_metadata_url=app.config['GOOGLE_DISCOVERY_URL'],
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+
+   # Redirect to google_auth function
+    redirect_uri = url_for('google_auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+
+
+@app.route('/google/auth')
+def google_auth():
+    try:
+        token = oauth.google.authorize_access_token()
+        app.logger.debug(str(token))
+    except Exception as ex:
+        app.logger.error(f"Error getting token: {ex}")
+        return redirect(url_for('lab11_login'))
+
+
+    app.logger.debug(str(token))
+
+
+    userinfo = token['userinfo']
+    app.logger.debug(" Google User " + str(userinfo))
+    email = userinfo['email']
+    try:
+        with db.session.begin():
+            user = (AuthUser.query.filter_by(email=email).with_for_update().first())
+
+
+            if not user:
+                name = userinfo['given_name'] + " " + userinfo['family_name']
+                random_pass_len = 8
+                password = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
+                                for i in range(random_pass_len))
+                picture = userinfo['picture']
+                new_user = AuthUser(email=email, name=name,
+                                    password=generate_password_hash(
+                                        password, method='sha256'),
+                                   avatar_url=picture)
+                db.session.add(new_user)
+                db.session.commit()
+    except Exception as ex:
+        db.session.rollback()  # Rollback on failure
+        app.logger.error(f"ERROR adding new user with email {email}: {ex}")
+        return redirect(url_for('lab11_login'))
+  
+    user = AuthUser.query.filter_by(email=email).first()
+    login_user(user)
+    return redirect('/lab10')
